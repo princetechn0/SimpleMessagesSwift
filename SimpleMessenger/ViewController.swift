8a992184ebcd6528e9dad8f3dfe6bae3099d125b
princetechn0 <13923942+princetechn0@users.noreply.github.com>
2020-01-12 16:47:29 -0800

    Initial Commit

---
+++ SimpleMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMessenger
+//
+//  Created by Ali Ataya on 1/12/20.
+//  Copyright © 2020 Ali Ataya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
